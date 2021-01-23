3a6909ab4a704e31cf6146f1f8c0d8b3095179c8
Mangelschots Vincent <vincent@mangelschots.org>
2021-01-23 08:18:09 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <Arduino.h>
+#include <Adafruit_ADS1015.h>
+
+
+
+Adafruit_ADS1015 ads = Adafruit_ADS1015(0x48); 
+
+void setup() {
+  
+  pinMode(2,OUTPUT);
+  Serial.begin(115200);
+  Serial.println("Single-ended readings from AIN0 with >3.0V comparator");
+  Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
+  Serial.println("Comparator Threshold: 1000 (3.000V)");
+  
+
+ 
+  // The ADC input range (or gain) can be changed via the following
+  // functions, but be careful never to exceed VDD +0.3V max, or to
+  // exceed the upper and lower limits if you adjust the input range!
+  // Setting these values incorrectly may destroy your ADC!
+  //                                                                ADS1015  ADS1115
+  //                                                                -------  -------
+   ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+  
+  ads.begin();
+  
+  // Setup 3V comparator on channel 0
+}
+
+void loop() {
+    int16_t adc0, adc1, adc2, adc3;
+    int8_t chars_read;
+    char buf[1024];
+  digitalWrite(2,HIGH);
+  delay(100);
+  digitalWrite(2,LOW);
+  delay(100);
+
+ 
+  adc0 = ads.readADC_SingleEnded(0);
+  adc1 = ads.readADC_SingleEnded(1);
+  adc2 = ads.readADC_SingleEnded(2);
+
+}
