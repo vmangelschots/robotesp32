c95bbdddf0d0d5997dae33c18a91fbe4786a0b10
Mangelschots Vincent <vincent@mangelschots.org>
2021-03-13 16:12:21 +0100

    verder gewerkt

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,57 @@
 #include <Arduino.h>
 #include <Adafruit_ADS1015.h>
-#include <WiFiManager.h>
 #include <WiFiUdp.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/semphr.h>
-
-
-
+#include <WiFiManager.h>
 
 
 Adafruit_ADS1015 ads = Adafruit_ADS1015(0x48); 
-WiFiManager wm;
 WiFiUDP udp;
 WiFiUDP udp_server;
 SemaphoreHandle_t lock;
-int servoPos[7];
+int servoPos[9];
 
 void TaskUpdateServos(void *pvParameters){
-
+  (void) pvParameters;
+  Serial.println("starting servo update task");
   for(;;){
-    if(xSemaphoreTake(lock,(10/portTICK_PERIOD_MS))){
-      Wire.beginTransmission(3);
-      Wire.write('S');
-      for(int i = 0; i<7;i++){
+    Serial.println("loop servo task");
+    if(xSemaphoreTake(lock,(10/portTICK_PERIOD_MS))==pdTRUE){
+      Serial.println("start transmitting servo pos");
+      Wire.beginTransmission(0x03);
+      Wire.write('S');  
+      for(int i = 0; i<9;i++){
         Wire.write(servoPos[i]);
       }
       Wire.endTransmission();
+      xSemaphoreGive(lock);
     }
-    vTaskDelay( 100 / portTICK_PERIOD_MS );
+    vTaskDelay( 10 / portTICK_PERIOD_MS );
   }
 }
 
 void TaskBlink(void *pvParameters)  // This is a task.
 {
   (void) pvParameters;
-
+  Serial.println("starting blink task");
   // initialize digital pin 2 as an output.
   pinMode(2, OUTPUT);
 
   for (;;) // A Task shall never return or exit.
   {
+    for(int i = 0; i<9;i++){
+        Serial.println(servoPos[i]);
+      }
     digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
     vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
     digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
     vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
   }
 }
-void TaskReceiveCommands(void *pvPArameters){
+void TaskReceiveCommands(void *pvParameters){
+  (void) pvParameters;
+  Serial.println("starting receive task");
   char packetBuffer[255]; 
   int packageSize=0;
   int len = 0;
@@ -60,19 +65,19 @@ void TaskReceiveCommands(void *pvPArameters){
         packetBuffer[len] = 0;
       }
       if(packetBuffer[0]=='S'){
-        if(len!=8){
+        if(len!=10){
           Serial.println("invallid servo command received");
         }
         else{
-          for(int i = 1; i<8;i++){
+          for(int i = 1; i<10;i++){
             servoPos[i-1] = packetBuffer[i];
           }
         }
       }
-      taskYIELD();
+       vTaskDelay( 10 / portTICK_PERIOD_MS ); // wait for one seconds
     }
     else{
-      vTaskDelay( 1 / portTICK_PERIOD_MS ); // wait for one seconds
+      vTaskDelay( 10 / portTICK_PERIOD_MS ); // wait for one seconds
     }
     
     
@@ -84,20 +89,22 @@ void TaskSendChannelData(void *pvParameters)  // This is a task.
   (void) pvParameters;
 
   
-
+  Serial.println("starting send channel data task");
   for (;;) // A Task shall never return or exit.
   {
     
     int16_t adc0, adc1, adc2;
-    if(xSemaphoreTake(lock,(10/portTICK_PERIOD_MS))){
-            adc0 = ads.readADC_SingleEnded(0);
-            adc1 = ads.readADC_SingleEnded(1);
-            adc2 = ads.readADC_SingleEnded(2);
+    if(xSemaphoreTake(lock,(10/portTICK_PERIOD_MS))==pdTRUE){
+      Serial.println("getting channel data");
+      adc0 = ads.readADC_SingleEnded(0);
+      adc1 = ads.readADC_SingleEnded(1);
+      adc2 = ads.readADC_SingleEnded(2);
+      xSemaphoreGive(lock);
     }
     else{
       continue;
     }
-    udp.beginPacket(IPAddress(192,168,5,173),5005);
+    udp.beginPacket(IPAddress(192,168,5,2),5005);
     udp.write('c');
     udp.write((uint8_t)(adc0 >> 8));
     udp.write((uint8_t)(adc0));
@@ -112,19 +119,9 @@ void TaskSendChannelData(void *pvParameters)  // This is a task.
 
 void setup() {
   
-  WiFi.mode(WIFI_STA);
+ 
   Serial.begin(115200);
-  bool res;
-  res = wm.autoConnect("Wall-E");
-  if(!res) {
-      Serial.println("Failed to connect");
-      // ESP.restart();
-  } 
-  else {
-      //if you get here you have connected to the WiFi    
-      Serial.println("connected...yeey :)");
-  }
-  
+
 
  
   // The ADC input range (or gain) can be changed via the following
@@ -141,13 +138,13 @@ void setup() {
   // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
   
   ads.begin();
-
+                                                                                                                                                                                                                                                                  
   /*set all servopos to 128 aka the middle*/ //TODO: kan dit niet in de init ?
-  for(int i=0; i<7;i++){
+  for(int i=0; i<9;i++){
     servoPos[i]=128;
   }
   lock = xSemaphoreCreateBinary();
-  Wire.begin(21,22);
+  xSemaphoreGive(lock);
   xTaskCreate(
     TaskBlink
     ,  "Blink"   // A name just for humans
@@ -156,26 +153,26 @@ void setup() {
     ,  tskIDLE_PRIORITY  // priority
     ,  NULL );
 
-    xTaskCreate(
+  xTaskCreate(
     TaskSendChannelData
     ,  "SendChannelData"   // A name just for humans
     ,  4096  // Stack size
     ,  NULL
     ,  tskIDLE_PRIORITY  // priority
     ,  NULL );
-    xTaskCreate(
+  xTaskCreate(
     TaskReceiveCommands
     ,  "ReceiveCommands"   // A name just for humans
     ,  4096  // Stack size
     ,  NULL
-    ,  5  // priority
+    ,  tskIDLE_PRIORITY  // priority
     ,  NULL );
-    xTaskCreate(
+  xTaskCreate(
     TaskUpdateServos
     ,  "TaskUpdateServos"   // A name just for humans
     ,  4096  // Stack size
     ,  NULL
-    ,  5  // priority
+    ,  tskIDLE_PRIORITY  // priority
     ,  NULL );
 }
 
